e2e86e5a1314b0e0452dfc8e0b0bd3b02f2452f7
biplob kumar <biplobkumar125@gmail.com>
2023-02-02 19:22:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+
+
+//  problem 3
+
+var mothergiven=2000;
+// 1kg=20tk
+var potato= parseFloat("40tk");
+// 1kg=40tk
+var tomato= parseFloat("40tk");
+// 1kg=120tk
+var sugar= parseFloat("360tk");
+// 2lg=200tk
+var mustardoil=parseFloat("1000tk")
+//  total 
+var total=potato+tomato+sugar+mustardoil;
+console.log("total="+ " "+total);
+//  farot 
+var farot =mothergiven-total;
+console.log("farot="+ " " +farot);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(mothergiven=total;mothergiven>=total; total+=1 ){
+//     if(b=mothergiven-total){
+//         console.log(b);
+//     }
+
+// }
+
+
+//  distrubite 20 blanket every person 1 blanket 
+// var blanket=20
+
+// for( blanket=1; blanket<=20;blanket++){
+// console.log( "blanket="+ " "+blanket);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
